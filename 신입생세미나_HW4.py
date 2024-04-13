e28e908f7d8d810ee65900edc124119076007058
gystat <166889985+gystat@users.noreply.github.com>
2024-04-13 12:49:10 +0900

    Create 신입생세미나_HW4.py

---
+++ 신입생세미나_HW4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        len_h = len(height)
+        if len_h <= 2:
+            return 0
+        max_h = max(height)
+        vol_bef = sum(height)
+
+        l = 0
+        max_l = 0
+        while height[l] < max_h:
+            if height[l] > max_l:
+                max_l = height[l]
+            height[l] = max_l
+            l += 1
+
+        r = len_h - 1
+        max_r = 0
+        while height[r] < max_h:
+            if height[r] > max_r:
+                max_r = height[r]
+            height[r] = max_r
+            r -= 1
+
+        vol_aft = sum(height[:l]) + sum(height[r:]) + (r - l) * max_h
+        return vol_aft - vol_bef
